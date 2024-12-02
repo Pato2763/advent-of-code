9e7b0568e02160224a44d1e4ff59f5f3e8c0196d
Pato2763 <daviespatrick2763@gmail.com>
2024-12-02 19:03:33 +0000

    completed the second part

--- Day-2/Red-Nosed-Reports.js
+++ Day-2/Red-Nosed-Reports.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const input = fs.readFileSync("Day-2/input.txt", "utf-8");
 
+//solution 1
+
 // turns the string into a nested array of each level
 const strToArray = (str) => {
   const arr = str.split("\n").map((arr) => {
@@ -72,6 +74,87 @@ const solution1 = (str) => {
   return totalSafe;
 };
 
-console.log(solution1(input));
+//solution 2
 
-module.exports = { solution1 };
+const tolerateBadLevelSafeCheck = (arr) => {
+  let safeCheck = 1;
+  let badLevel = false;
+  let i = 1;
+
+  //checks for ascending
+  let arrAsc = [...arr];
+  while (i < arrAsc.length && safeCheck === 1) {
+    if (arrAsc[i] - arrAsc[i - 1] > 3 || arrAsc[i] - arrAsc[i - 1] <= 0) {
+      if (badLevel) {
+        safeCheck = 0;
+      } else {
+        badLevel = true;
+        arrAsc.splice(i, 1);
+      }
+    } else {
+      i++;
+    }
+  }
+
+  badLevel = false;
+
+  //checks for descending
+
+  if (!safeCheck) {
+    i = 1;
+    let arrDesc = [...arr];
+    safeCheck = 1;
+    while (i < arrDesc.length && safeCheck === 1) {
+      if (arrDesc[i - 1] - arrDesc[i] > 3 || arrDesc[i - 1] - arrDesc[i] <= 0) {
+        if (badLevel) {
+          safeCheck = 0;
+        } else {
+          badLevel = true;
+          arrDesc.splice(i, 1);
+        }
+      } else {
+        i++;
+      }
+    }
+  }
+
+  return safeCheck;
+};
+
+const isSafewithDampner = (level) => {
+  const removedCheck = (levelSpliced) => {
+    let isAscending = null;
+    for (i = 1; i < levelSpliced.length; i++) {
+      let dif = levelSpliced[i] - levelSpliced[i - 1];
+      if (Math.abs(dif) > 3 || Math.abs(dif) === 0) return false;
+      if (isAscending === null) isAscending = dif > 0;
+
+      if ((isAscending && dif < 0) || (!isAscending && dif > 0)) return false;
+    }
+    return true;
+  };
+
+  for (let i = 0; i < level.length; i++) {
+    let arr = [...level];
+    arr.splice(i, 1);
+    if (removedCheck(arr)) return true;
+  }
+  return false;
+};
+
+const solution2 = (str) => {
+  const arr = strToArray(str);
+  let totalSafe = 0;
+  arr.forEach((singleArr) => {
+    if (singleLevelSafeCheck(singleArr) === 1 || isSafewithDampner(singleArr)) {
+      totalSafe++;
+    }
+  });
+
+  return totalSafe;
+};
+
+console.log(solution1(input));
+console.log(solution2(input));
+
+module.exports = { solution1, solution2 };
--- Day-2/Red-Nosed-Reports.test.js
+++ Day-2/Red-Nosed-Reports.test.js
@@ -1,9 +1,9 @@
-const { solution1 } = require("./Red-Nosed-Reports");
+const { solution1, solution2 } = require("./Red-Nosed-Reports");
 
 describe("determine safetey of different levels", () => {
   test("should deem it unsafe if it increases and decreases", () => {
     const input1 = `1 3 2 5 6`; //unsafe case asc to desc
-    const input2 = `6 5 6 3 2`;
+    const input2 = `6 5 6 3 2`; //unsafe case desc to asc
     expect(solution1(input1)).toBe(0);
     expect(solution1(input2)).toBe(0);
   });
@@ -11,13 +11,11 @@ describe("determine safetey of different levels", () => {
     const input1 = `1 3 6 8 9`; // safe case
     const input2 = `1 5 6 8 9`; // unsafe case
     const input3 = `1 3 3 4 5`; // unsafe case
-    const input4 = `11 13 15 18 19 20`;
     expect(solution1(input1)).toBe(1);
     expect(solution1(input2)).toBe(0);
     expect(solution1(input3)).toBe(0);
-    expect(solution1(input4)).toBe(1);
   });
-  test("determine if a single level is safe when the numbers are descendibg by increments of 1 and 3", () => {
+  test("determine if a single level is safe when the numbers are descending by increments of 1 and 3", () => {
     const input1 = `9 8 6 3 2 1`; // safe case
     const input2 = `9 8 4 3 2 1`; // unsafe case
     const input3 = `9 8 6 3 3 1`; // unsafe case
@@ -33,5 +31,34 @@ describe("determine safetey of different levels", () => {
 8 6 4 4 1
 1 3 6 7 9`;
     expect(solution1(input)).toBe(2);
+  });
+});
+
+describe("problem dampner", () => {
+  test("without removing any level it should deem if it's safe or not", () => {
+    const input1 = `1 3 6 8 9`; // safe case
+    const input2 = `9 8 6 3 2 1`; // safe case
+
+    expect(solution2(input1)).toBe(1);
+    expect(solution2(input2)).toBe(1);
+  });
+  test("should allow for one bad level", () => {
+    const input1 = `1 2 3 3 6`; // safe case
+    const input2 = `1 2 2 2 3 5`; // unsafe case
+    const input3 = `1 2 1 3 4 3`; //unsafe case
+
+    expect(solution2(input1)).toBe(1);
+    expect(solution2(input2)).toBe(0);
+    expect(solution2(input3)).toBe(0);
+  });
+  test("should allow for multiple reports", () => {
+    const input = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
+
+    expect(solution2(input)).toBe(4);
   });
 });
